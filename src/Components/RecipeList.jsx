eb47c019a6afb1664d3e3b7e982af958139baa05
nechami795 <nechami795@gmail.com>
2025-02-04 18:18:40 +0200

    responsivi

--- src/Components/RecipeList.jsx
+++ src/Components/RecipeList.jsx
@@ -8,6 +8,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
+import Grid from '@mui/material/Grid';
 
 const RecipeList = () => {
     const recipes = useSelector((state) => state.recipeSlice.Recipes);
@@ -15,96 +16,94 @@ const RecipeList = () => {
         console.log(recipes);
     }, [recipes]);
 
-    return (<>
-  <Box sx={{ position: 'relative', width: '74.2vw', height: '55vh', marginRight: '12.5%', marginTop: '2vh', textAlign: 'center' }}>
-    <img 
-        src="/Images/macaron.jpg" 
-        style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
-    />
-    <Typography 
-        sx={{ 
-            position: 'absolute', 
-            top: '50%', 
-            left: '50%', 
-            color: 'white', 
-            fontSize: '100px',
-            transform: 'translate(-50%, -50%)' 
-        }}
-    >
-        מתכונים
-    </Typography>
-</Box>
+    return (
+        <>
+            <Box sx={{ position: 'relative', width: '74.2vw', height: '55vh', marginRight: '12.5%', marginTop: '2vh', textAlign: 'center' }}>
+                <img 
+                    src="/Images/macaron.jpg" 
+                    style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+                />
+                <Typography 
+                    sx={{ 
+                        position: 'absolute', 
+                        top: '50%', 
+                        left: '50%', 
+                        color: 'white', 
+                        fontSize: '100px',
+                        transform: 'translate(-50%, -50%)' 
+                    }}
+                >
+                    מתכונים
+                </Typography>
+            </Box>
 
-
-      
-
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-
-            <ImageList sx={{ width: '75%', height: 'auto', overflow: 'hidden' }} gap={10} cols={3} rowHeight={500}>
-                {recipes.map((item) => (
-                    <ImageListItem key={item.id} sx={{ position: 'relative' }}>
-                        <Link to={`/RecipeList/${item.id}`} style={{
-                            display: 'block',
-                            width: '100%',
-                            height: '100%',
-                            textDecoration: 'none'
-                        }}>
-                            <img
-                                src={item.img}
-                                alt={item.name}
-                                loading="lazy"
-                                style={{
-                                    width: '100%', height: '100%', objectFit: 'cover', maskImage: "radial-gradient(circle, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 100%)",
-                                    WebkitMaskImage: "radial-gradient(circle, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 100%)"
-                                }}
-                            />
-                            <ImageListItemBar
-                                sx={{
-                                    background: 'linear-gradient(to top, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.5) 50%, rgba(0,0,0,0) 100%)',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'flex-start',
-                                    alignItems: 'flex-start',
-                                    position: 'absolute',
-                                    bottom: 0,
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Grid container spacing={2} sx={{ width: '75%', height: 'auto' }}>
+                    {recipes.map((item) => (
+                        <Grid item xs={12} sm={6} md={4} key={item.id}>
+                            <ImageListItem sx={{ position: 'relative' }}>
+                                <Link to={`/RecipeList/${item.id}`} style={{
+                                    display: 'block',
                                     width: '100%',
-                                }}
-                                position="bottom"
-                                actionIcon={
-                                    <Box sx={{ color: 'white', width: '100%', paddingRight: '7px' }}>
-                                        <Typography variant="h6" sx={{ marginBottom: '8px', fontWeight: 'bold', fontSize: '25px' }}>
-                                            {item.name}
-                                        </Typography>
-
-                                        <Box sx={{
+                                    height: '100%',
+                                    textDecoration: 'none'
+                                }}>
+                                    <img
+                                        src={item.img}
+                                        alt={item.name}
+                                        loading="lazy"
+                                        style={{
+                                            width: '100%', height: '100%', objectFit: 'cover', maskImage: "radial-gradient(circle, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 100%)",
+                                            WebkitMaskImage: "radial-gradient(circle, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 100%)"
+                                        }}
+                                    />
+                                    <ImageListItemBar
+                                        sx={{
+                                            background: 'linear-gradient(to top, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.5) 50%, rgba(0,0,0,0) 100%)',
                                             display: 'flex',
-                                            marginBottom: '8px',
-                                            gap: '8px',
-                                            alignItems: 'center',
-                                        }}>
-                                            <AccessTimeIcon sx={{ fontSize: 20 }} />
-                                            <Typography variant="body2" sx={{ fontSize: '14px' }}>{item.time}</Typography>
-                                        </Box>
+                                            flexDirection: 'column',
+                                            justifyContent: 'flex-start',
+                                            alignItems: 'flex-start',
+                                            position: 'absolute',
+                                            bottom: 0,
+                                            width: '100%',
+                                        }}
+                                        position="bottom"
+                                        actionIcon={
+                                            <Box sx={{ color: 'white', width: '100%', paddingRight: '7px' }}>
+                                                <Typography variant="h6" sx={{ marginBottom: '8px', fontWeight: 'bold', fontSize: '25px' }}>
+                                                    {item.name}
+                                                </Typography>
 
-                                        <Box sx={{
-                                            display: 'flex',
-                                            gap: '8px',
-                                            alignItems: 'center',
-                                        }}>
-                                            <RestaurantIcon sx={{ fontSize: 20 }} />
-                                            <Typography variant="body2" sx={{ fontSize: '14px' }}>{item.category}</Typography>
-                                        </Box>
-                                    </Box>
-                                }
-                                actionPosition="left"
-                            />
-                        </Link>
+                                                <Box sx={{
+                                                    display: 'flex',
+                                                    marginBottom: '8px',
+                                                    gap: '8px',
+                                                    alignItems: 'center',
+                                                }}>
+                                                    <AccessTimeIcon sx={{ fontSize: 20 }} />
+                                                    <Typography variant="body2" sx={{ fontSize: '14px' }}>{item.time}</Typography>
+                                                </Box>
 
-                    </ImageListItem>
-                ))}
-            </ImageList>
-        </Box>
-    </>
+                                                <Box sx={{
+                                                    display: 'flex',
+                                                    gap: '8px',
+                                                    alignItems: 'center',
+                                                }}>
+                                                    <RestaurantIcon sx={{ fontSize: 20 }} />
+                                                    <Typography variant="body2" sx={{ fontSize: '14px' }}>{item.category}</Typography>
+                                                </Box>
+                                            </Box>
+                                        }
+                                        actionPosition="left"
+                                    />
+                                </Link>
+                            </ImageListItem>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </>
     );
 };
 
